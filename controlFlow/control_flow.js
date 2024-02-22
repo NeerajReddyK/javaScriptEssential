67c396be0e529388d1a2af5ebaacd623a2f3b4c2
Neeraj Reddy <neerajreddy0059@gmail.com>
2024-02-22 08:51:06 -0500

    Fixed a typo error which was making the app break

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -46,5 +46,5 @@ switch (userType) {
 console.log("User Category:", userCategory);
 
 let isAuthenticated = true;
-let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";\
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 console.log("Authentication Status:", authenticationStatus);
